4cf3f645edbc5d1c9348684b50d1f708bc091be0
vlaDio <vladyevchuk07@gmail.com>
2025-09-09 12:19:37 +0300

    Update sprite-loader.js

--- assets/JS/sprite-loader.js
+++ assets/JS/sprite-loader.js
@@ -1,8 +1,9 @@
-fetch("../img/icons/sprite.svg")
+fetch("./assets/img/icons/sprite.svg")
   .then(r => r.text())
   .then(svg => {
     document.getElementById("svg-sprite").innerHTML = svg;
   })
   .catch(err => console.error("SVG sprite load failed:", err));
+
 
 
