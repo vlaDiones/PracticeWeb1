cd4cd00429e435a37e8852d3c5b8dc327a5c9615
vlaDio <vladyevchuk07@gmail.com>
2025-09-09 12:07:41 +0300

    Delete Interiors directory

--- Interiors/assets/JS/sprite-loader.js
+++
@@ -1,6 +0,0 @@
-fetch("./assets/img/icons/sprite.svg")
-  .then(r => r.text())
-  .then(svg => {
-    document.getElementById("svg-sprite").innerHTML = svg;
-  })
-  .catch(err => console.error("SVG sprite load failed:", err));
