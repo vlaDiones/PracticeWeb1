e4f791c3ac3081de191925cf590ad2abca9e9099
vlaDio <vladyevchuk07@gmail.com>
2025-09-09 12:40:10 +0300

    Update sprite-loader.js

--- assets/JS/sprite-loader.js
+++ assets/JS/sprite-loader.js
@@ -1,9 +1,10 @@
-fetch("/PracticeWeb1/assets/img/icons/sprite.svg")
+fetch("assets/img/icons/sprite.svg")
   .then(r => r.text())
   .then(svg => {
     document.getElementById("svg-sprite").innerHTML = svg;
   })
   .catch(err => console.error("SVG sprite load failed:", err));
+
 
 
 
