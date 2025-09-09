fa5cfcfe9cf52c3dae9d207ac06f702822140136
vlaDio <vladyevchuk07@gmail.com>
2025-09-09 12:08:41 +0300

    Add files via upload

---
+++ assets/JS/sprite-loader.js
@@ -0,0 +1,6 @@
+fetch("./assets/img/icons/sprite.svg")
+  .then(r => r.text())
+  .then(svg => {
+    document.getElementById("svg-sprite").innerHTML = svg;
+  })
+  .catch(err => console.error("SVG sprite load failed:", err));
