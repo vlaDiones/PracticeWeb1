09fbdc228ca931792aa6b3a80894db54e0aeed09
vlaDio <vladyevchuk07@gmail.com>
2025-09-12 09:02:13 +0300

    Update scripts.js

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -74,7 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
         
         const scrollDistance = 8000; 
         
-        const stickyPoint = header.offsetTop;
+        const stickyPoint = header.offsetTop - 10;
 
         const lerpColor = (colorA, colorB, amount) => {
             const r = Math.round(colorA[0] + (colorB[0] - colorA[0]) * amount);
@@ -97,4 +97,5 @@ document.addEventListener("DOMContentLoaded", () => {
         window.addEventListener('scroll', handle_scroll);
     }    
 });
+
 
